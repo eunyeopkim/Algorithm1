7d3121a20f90fd323f4c6db4191ac6ede928ba50
yeops <dmsduqzz@gmail.com>
2023-05-27 17:18:22 +0900

    [20230527] programmers 적응기...

---
+++ src/Programmers/Average.java
@@ -0,0 +1,11 @@
+package Programmers;
+class Average {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int i=0; i<arr.length; i++) {
+            answer += arr[i];
+        }
+        answer = answer/arr.length;
+        return answer;
+    }
+}
